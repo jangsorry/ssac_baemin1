810c9121a07bbca59c3262e1d95bd86e9c75a6de
jangsorry <piccolox@naver.com>
2021-10-16 21:12:30 +0900

    Initial Commit

---
+++ ssac_baemin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ssac_baemin
+//
+//  Created by 지성 on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
